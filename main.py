c89cb82964957e8e6be896dce8818f6793665b3c
egzon-baruti <63745689+xoniks@users.noreply.github.com>
2023-11-28 20:44:55 +0100

    updated main

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import streamlit as st
 
 st.title('Hello to our wage app :sunglasses:')
 
-hours = st.number_input('Insert a hours')
+hours = st.number_input('Insert a hours', step=1)
 rates = st.number_input('Insert a rate')
 
 
 def calculate_wage(hour, rate):
     return hour * rate
 
-result = calculate_wage(hours,rates)
 
-
-st.write('The current number is ', result)
+if st.button('Calculate wage!'):
+    result = calculate_wage(hours,rates)
+    st.write('The current number is ', result)
