212d7fc48582a2074e55a2d0b292d25709f9c74d
egzon-baruti <63745689+xoniks@users.noreply.github.com>
2023-11-30 15:57:17 +0100

    json

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import streamlit as st
+import json
 
-# Initialize parking lot status using session state
-if 'parking_lots' not in st.session_state:
-    st.session_state.parking_lots = {f"Lot {i + 1}": None for i in range(10)}
+# Load or initialize parking lot status from a JSON file
+PARKING_LOTS_FILE = "parking_lots.json"
+
+def load_parking_lots():
+    try:
+        with open(PARKING_LOTS_FILE, "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return {f"Lot {i + 1}": None for i in range(10)}
+
+def save_parking_lots(parking_lots):
+    with open(PARKING_LOTS_FILE, "w") as file:
+        json.dump(parking_lots, file)
+
+# Initialize parking lot status
+parking_lots = load_parking_lots()
 
 def parking_app():
     st.title("Parking App")
@@ -17,17 +31,18 @@ def parking_app():
 
     # Display available parking lots
     st.subheader("Available Parking Lots")
-    available_lots = [lot for lot, status in st.session_state.parking_lots.items() if status is None]
+    available_lots = [lot for lot, status in parking_lots.items() if status is None]
     selected_lot = st.selectbox("Select Parking Lot", available_lots)
 
     # Book parking lot
     if st.button("Book Parking Lot"):
-        st.session_state.parking_lots[selected_lot] = worker_name
+        parking_lots[selected_lot] = worker_name
+        save_parking_lots(parking_lots)
         st.success(f"{worker_name} successfully booked {selected_lot}.")
 
     # Display booked parking lots
     st.subheader("Booked Parking Lots")
-    booked_lots = {lot: worker for lot, worker in st.session_state.parking_lots.items() if worker is not None}
+    booked_lots = {lot: worker for lot, worker in parking_lots.items() if worker is not None}
     if not booked_lots:
         st.info("No parking lots have been booked yet.")
     else:
